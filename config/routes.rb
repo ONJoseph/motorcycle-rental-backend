b010c48cdfb0eea850fdc75095b5e5cee983c19c
ONJoseph <on.josephinfo@gmail.com>
2023-05-30 17:01:46 +0100

    Add models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+
+  # post '/api/v1/login', to: 'accounts#create'
+  # post '/api/v1/logout', to: 'accounts#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get "/reservations/:name", to: 'reservations#index'
+      resources :users, only: [:index, :create] 
+      resources :reservations, only: [:index, :create] 
+      resources :vehicles, only: [:index, :show, :create, :destroy] 
+      resources :accounts, only: [:create]
+    end 
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
