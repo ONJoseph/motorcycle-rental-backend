911fdee3c62c363ac0874684dfc56ed9e44a57c5
ONJoseph <on.josephinfo@gmail.com>
2023-05-31 21:15:47 +0100

    Add registrations controller

---
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -0,0 +1,20 @@
+class ApplicationController < ActionController::API
+end
+
+class Api::V1::Users::RegistrationsController < ApplicationController
+  def create
+    user = User.new(sign_up_params)
+
+    if user.save
+      render json: { user: user, message: 'Signed up successfully', is_success: true }, status: :ok
+    else
+      render json: { message: 'Sign up failed', is_success: false }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def sign_up_params
+    params.permit(:name)
+  end
+end
