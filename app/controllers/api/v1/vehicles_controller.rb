beb4a14ff141e73f699ef67a2a1a6ab1a656d22b
ONJoseph <on.josephinfo@gmail.com>
2023-06-01 00:14:25 +0100

    rails generate controller Vehicles

---
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -0,0 +1,54 @@
+class Api::V1::VehiclesController < ApplicationController
+  before_action :set_vehicle, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @vehicles = Vehicle.all
+    render json: @vehicles
+  end
+
+  def show
+    render json: @vehicle
+  end
+
+  def new
+    @vehicle = Vehicle.new
+    render json: @vehicle
+  end
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+
+    if @vehicle.save
+      render json: @vehicle, status: :created
+    else
+      render json: { errors: @vehicle.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    render json: @vehicle
+  end
+
+  def update
+    if @vehicle.update(vehicle_params)
+      render json: @vehicle
+    else
+      render json: { errors: @vehicle.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @vehicle.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_vehicle
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  def vehicle_params
+    params.require(:vehicle).permit(:model, :picture, :description, :price, :brand, :year, :color, :registration_plate, :accidents)
+  end
+end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,13 +1,13 @@
 class Vehicle < ApplicationRecord
   has_many :reservations, dependent: :destroy
   validates :model, presence: true, length: { maximum: 250 }
-  validates :image, presence: true
+  validates :picture, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :accidents, numericality: { greater_than_or_equal_to: 0 }
 
   def as_json(options = {})
     super(options.merge(
-      only: %i[id model image description price brand year color accidents is_electric]
+      only: %i[id model picture description price brand year color accidents registration_plate]
     ))
   end
 end
