182aca41c324c52e153ac03ed1c932f63c8eca5d
ONJoseph <on.josephinfo@gmail.com>
2023-05-31 21:16:44 +0100

    Delete accounts controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,16 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
 
-  # post '/api/v1/login', to: 'accounts#create'
-  # post '/api/v1/logout', to: 'accounts#destroy'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      get "/reservations/:name", to: 'reservations#index'
-      resources :users, only: [:index, :create] 
-      resources :reservations, only: [:index, :create] 
-      resources :vehicles, only: [:index, :show, :create, :destroy] 
-      resources :accounts, only: [:create]
-    end 
-  end
+      post 'signup', to: 'users/registrations#create'
+      post 'login', to: 'users/sessions#create'
+      delete 'logout', to: 'users/sessions#destroy'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+      resources :vehicles
+      resources :users, only: [:index] do
+        resources :reservations, only: [:create, :index, :destroy, :show, :update]
+      end
+    end
+  end
 end
